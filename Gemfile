9f67aff9f9c4dd7f6a928e925490c28fa3b71e9d
Kevin Wu <me@kevinformatics.com>
2015-10-30 00:56:22 -0700

    hello world

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'middleman', '>= 4.0.0.rc.1'
+gem 'middleman-sprockets', '>= 4.0.0.rc.1'
+gem 'middleman-livereload'
+
+gem 'slim'
+gem 'bootstrap-sass', '~> 3.3.5', require: false
---
+++ source/javascripts/triangles.js
@@ -0,0 +1,90 @@
+//= require "tessellate/tessellate.js"
+//= require "randomcolor/randomColor.js"
+
+var generateTriangles = function(opts) {
+
+  args = {
+    size: opts.size || 25,
+    height: opts.height || window.height,
+    width: opts.width || window.width,
+    randomStart: false || opts.randomStart
+  }
+
+  if(opts.randomColors)
+    args['colors'] = randomColor({count: 20, luminosity: 'light'})
+  else
+    args['colors'] = ["#f3f3f3", "#f0f0f0", "#ebebeb", "#e6e6e6", "#e2e2e2"]
+
+  return $(tessellate(args))
+
+}
+
+var trianglize = function(opts) {
+
+  opts = opts || {}
+
+  $('.landing-background').remove()
+  landingOpts = $.extend({
+    randomStart: true,
+    width: window.width * 0.75,
+    height: window.height * 0.75}
+  , opts)
+
+  var triangles = generateTriangles(landingOpts).addClass('landing-background')
+  $('.js-landing-background').append(triangles);
+  triangles = null
+
+  $('.triangle-hr').remove()
+  var hrOpts = $.extend({size: 10, height: 10}, opts)
+  $('.js-triangle-hr').each(function(i, el) {
+    var triangleHr = generateTriangles(hrOpts).addClass('triangle-hr')
+    $(el).append(triangleHr)
+    triangleHr = null
+  });
+}
+
+$(window).resize(function() {
+  trianglize()
+})
+
+$(window).scroll(function() {
+  trianglize({randomColors: true})
+  $('.js-logo-scroll').addClass('logo-scroll')
+
+  // add the 'logo-scroll' class that changes the initials to an outline
+  // during scroll, remove with a setTimeout
+  clearTimeout($.data(this, 'scrollTimer'));
+  $.data(this, 'scrollTimer', setTimeout(function() {
+    // restore triangle color and outline logo when scroll finish
+    trianglize()
+    $('.js-logo-scroll').removeClass('logo-scroll')
+  }, 100));
+});
+
+$(document).ready(function() {
+
+  // initialize triangles
+  trianglize()
+
+  // when landing is clicked, scroll to start of content
+  $('.js-triangles').click(function() {
+    $('html, body').animate({
+      scrollTop: $(".js-content-start").offset().top
+    }, 1500);
+  })
+
+  // anchor tag hover random color
+  var anchorHoverIntervalId = null;
+  $('a').hover(
+    function() {
+      var $hoveredLink = $(this)
+      anchorHoverIntervalId = setInterval(function() {
+        $hoveredLink.css('color', randomColor({luminosity: 'dark'}))
+      }, 100)
+    }, function() {
+      $(this).css('color', '#333')
+      clearInterval(anchorHoverIntervalId)
+      anchorHoverIntervalId = null;
+    }
+  )
+})
